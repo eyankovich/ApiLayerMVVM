625ac7747ee6738df88a68bf03c88094b089d3a5
Egor <superzzzzet@gmail.com>
2021-01-27 12:25:45 +0300

    Initial Commit

---
+++ ApiLayerMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApiLayerMVVM
+//
+//  Created by Егор Янкович on 1/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
